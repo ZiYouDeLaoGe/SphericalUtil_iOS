0c1e73b289dd13d8611b258edb53427ef6091147
李仁兵 <ziyoudelaoge@163.com>
2015-11-13 19:24:37 +0800

    Method Description

--- MapLine/SphericalUtil/SphericalUtil+ComputeArea.h
+++ MapLine/SphericalUtil/SphericalUtil+ComputeArea.h
@@ -2,6 +2,8 @@
 //  SphericalUtil+ComputeArea.h
 //  MapLine
 //
+//  Calculate the area based on latitude and longitude
+//
 //  Created by 李仁兵 on 15/11/13.
 //  Copyright © 2015年 李仁兵. All rights reserved.
 //
@@ -9,5 +11,13 @@
 #import "SphericalUtil.h"
 
 @interface SphericalUtil (ComputeArea)
+
+/*!
+ @method - (CGFloat)computeArea:(CLLocationCoordinate2D *)coordinate2Ds andCount:(NSInteger)count;
+ @abstract Calculate the area based on latitude and longitude
+ @param coordinate2Ds:CLLocationCoordinate2D *;Coordinates array
+ @param count:NSInteger;Coordinates array number
+ @return area.If the number is less than 3 latitude and longitude returns 0, otherwise it returns the correct area
+ */
 - (CGFloat)computeArea:(CLLocationCoordinate2D *)coordinate2Ds andCount:(NSInteger)count;
 @end
